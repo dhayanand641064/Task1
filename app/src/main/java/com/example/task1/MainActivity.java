a44d5520f4967537d57d6616cebeb1bdf54ea278
tisf801@gmail.com <tisf801@gmail.com>
2022-05-31 20:40:33 +0530

    Task 1

--- app/src/main/java/com/example/task1/MainActivity.java
+++ app/src/main/java/com/example/task1/MainActivity.java
@@ -111,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 num1.setText(a);
-                a = " ";
+                a = null;
                 count = count+1;
             }
         });
@@ -119,7 +119,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 num2.setText(a);
-                a = " ";
+                a = null;
                 count = count+1;
             }
         });
@@ -159,7 +159,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 num3.setText(a);
-                a = " ";
+                a = null;
                 count = count+1;
             }
         });
@@ -167,7 +167,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 num4.setText(a);
-                a = " ";
+                a = null;
                 count = count+1;
             }
         });
@@ -175,7 +175,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 num5.setText(a);
-                a = " ";
+                a = null;
                 count = count+1;
             }
         });
@@ -183,7 +183,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 num6.setText(a);
-                a = " ";
+                a = null;
                 count = count+1;
             }
         });
@@ -191,7 +191,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 num7.setText(a);
-                a = " ";
+                a = null;
                 count = count+1;
             }
         });
@@ -199,7 +199,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 num8.setText(a);
-                a = " ";
+                a = null;
                 count = count+1;
             }
         });
@@ -225,13 +225,57 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d("abcde"," coming here 123");
-                if(count<8){
+                if(count<8 ){
                     Toast.makeText(MainActivity.this,"Please fill all the values", Toast.LENGTH_LONG).show();
                 }
                 else if (Integer.parseInt((String) num1.getText())+Integer.parseInt((String) num2.getText()) == p+q & Integer.parseInt((String) num3.getText())-Integer.parseInt((String) num4.getText()) == p1-q1 & Integer.parseInt((String) num5.getText())*Integer.parseInt((String) num6.getText()) == p2*q2 & Integer.parseInt((String) num7.getText())%Integer.parseInt((String) num8.getText()) == p3%q3)
                 {
                     score = score+1;
                     //Toast.makeText(MainActivity.this,"score:+{score}", Toast.LENGTH_LONG).show();
+                    num1.setText(null);
+                    num2.setText(null);
+                    p = rand.nextInt(100);
+                    q = rand.nextInt(100);
+                    option1.setText(String.valueOf(p));
+                    option2.setText(String.valueOf(q));
+                    result1.setText(String.valueOf(p+q));
+                    Score.setText(String.valueOf(score));
+                    //Toast.makeText(MainActivity.this,"score:+{score}", Toast.LENGTH_LONG).show();
+                    num3.setText(null);
+                    num4.setText(null);
+                    p1 = rand.nextInt(100);
+                    q1 = rand.nextInt(100);
+                    option3.setText(String.valueOf(p1));
+                    option4.setText(String.valueOf(q1));
+                    result2.setText(String.valueOf(p1-q1));
+                    Score.setText(String.valueOf(score));
+                    num5.setText(null);
+                    num6.setText(null);
+                    num7.setText(null);
+                    num8.setText(null);
+                    p2 = rand.nextInt(100);
+                    q2 = rand.nextInt(100);
+                    option5.setText(String.valueOf(p2));
+                    option6.setText(String.valueOf(q2));
+                    result3.setText(String.valueOf(p2*q2));
+                    Score.setText(String.valueOf(score));
+                    option7.setText(String.valueOf(p3));
+                    option8.setText(String.valueOf(q3));
+                    result4.setText(String.valueOf(p3%q3));
+                    Score.setText(String.valueOf(score));
+                    option1.setEnabled(true);
+                    option2.setEnabled(true);
+                    option3.setEnabled(true);
+                    option4.setEnabled(true);
+                    option5.setEnabled(true);
+                    option6.setEnabled(true);
+                    option7.setEnabled(true);
+                    option8.setEnabled(true);
+
+            }
+                else{
+                    Toast.makeText(MainActivity.this,"Ur ans is wrong,try again", Toast.LENGTH_LONG).show();
+                    r =r-1;
                     num1.setText(" ");
                     num2.setText(" ");
                     p = rand.nextInt(100);
@@ -251,6 +295,8 @@ public class MainActivity extends AppCompatActivity {
                     Score.setText(String.valueOf(score));
                     num5.setText(" ");
                     num6.setText(" ");
+                    num7.setText(" ");
+                    num8.setText(" ");
                     p2 = rand.nextInt(100);
                     q2 = rand.nextInt(100);
                     option5.setText(String.valueOf(p2));
@@ -261,11 +307,15 @@ public class MainActivity extends AppCompatActivity {
                     option8.setText(String.valueOf(q3));
                     result4.setText(String.valueOf(p3%q3));
                     Score.setText(String.valueOf(score));
+                    option1.setEnabled(true);
+                    option2.setEnabled(true);
+                    option3.setEnabled(true);
+                    option4.setEnabled(true);
+                    option5.setEnabled(true);
+                    option6.setEnabled(true);
+                    option7.setEnabled(true);
+                    option8.setEnabled(true);
 
-            }
-                else{
-                    Toast.makeText(MainActivity.this,"Ur ans is wrong,try again", Toast.LENGTH_LONG).show();
-                    r =r-1;
                     if(r==0){
                         Toast.makeText(MainActivity.this,"U lost all ur lives so start from first", Toast.LENGTH_LONG).show();
                         Score.setText("0");
